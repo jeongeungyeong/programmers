b7f0062598554ce77acd5292c505e540e17d5980
jeongeungyeong <150095257+jeongeungyeong@users.noreply.github.com>
2024-11-30 06:40:19 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.04 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,17 +1,23 @@
 class Solution {
     public int solution(int num) {
-        long n = (long) num;
-        int count = 0; 
+        int answer = 0;
+        long n = num;
         
-        while(n != 1 && count < 500){
-        if(n%2==0){
-           n /= 2;
-        } else{
-           n = (n*3)+1;
-        }
-            count++;
+        // num이 1이 아니고, answer가 500 이하일 때
+        while (n != 1){
+            if(answer >= 500){
+                return -1;
+            }
+            if (n % 2 == 0){
+                n = n/2;
+            }
+            else {
+                n = n * 3 +1;
+            }
+            
+            answer++;
         }
         
-        return (n==1) ? count : -1;
+        return answer;
     }
 }
