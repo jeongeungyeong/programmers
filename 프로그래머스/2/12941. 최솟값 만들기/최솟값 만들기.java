a708cd871f5f788ea9b67a12e10ef0fabe601aca
jeongeungyeong <150095257+jeongeungyeong@users.noreply.github.com>
2024-12-02 05:38:34 +0900

    [level 2] Title: 최솟값 만들기, Time: 3.10 ms, Memory: 53.4 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -1,29 +1,23 @@
-import java.util.*;
+import java.util.Arrays;
 
 class Solution
 {
     public int solution(int []A, int []B)
     {
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>(); // 우선순위 큐 최소힙
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder()); // 우선순위 큐 최대 힙
-        
-        // 배열 A를 최소힙에 다 넣음
-        for(int num : A){
-            minHeap.offer(num);
-        }
-        
-        // 배열 B를 최대힙에 다 넣음
-        for (int num : B){
-            maxHeap.offer(num);
-        }
-        
         int answer = 0;
+        // 배열의 길이
+        int length = A.length;
         
-        while(!minHeap.isEmpty()){
-            answer += minHeap.poll() * maxHeap.poll();
+        // 배열 A,B 오름차순 정렬
+        Arrays.sort(A);
+        Arrays.sort(B);
+        
+        // A[i] * B[length-i] = 최소
+        for(int i =0; i<length; i++){
+            answer += A[i] * B[length-1-i];
         }
-        
-        System.out.print(answer);
+      
+
         return answer;
     }
 }
