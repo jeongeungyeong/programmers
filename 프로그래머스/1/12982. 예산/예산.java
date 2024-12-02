bcc3c86393516341160199e6578bb7bb2e69f9f5
jeongeungyeong <150095257+jeongeungyeong@users.noreply.github.com>
2024-12-03 08:40:31 +0900

    [level 1] Title: 예산, Time: 1.23 ms, Memory: 89 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.java
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -1,21 +1,24 @@
-import java.util.*;
+import java.util.Arrays;
 
 class Solution {
     public int solution(int[] d, int budget) {
-        int total = 0;
-        int count = 0;
-        Arrays.sort(d); // 오름차순 정렬
+        int answer = 0;
+        // 예산의 합
+        int sum = 0;
         
+        // 배열 d 오름차순 정렬
+        Arrays.sort(d);
         
-        for(int i : d){
-            if(i + total <= budget){
-                total += i;
-                count ++;
-            } else{
+        // 순차적으로 더하기
+        for(int amount : d){
+            // 만일 합이 예산을 초과하면 멈추기
+            if(sum+amount > budget){
                 break;
             }
+            sum += amount;
+            answer++; // 부서 횟수 증가
         }
         
-        return count;
+        return answer;
     }
 }
