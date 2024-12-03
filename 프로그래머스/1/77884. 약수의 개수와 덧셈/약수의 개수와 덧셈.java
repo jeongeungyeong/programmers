d6d45e78dc07906a18283088709e43de9c8f6aaa
jeongeungyeong <150095257+jeongeungyeong@users.noreply.github.com>
2024-12-03 23:16:20 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.45 ms, Memory: 84.8 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
@@ -1,15 +1,12 @@
 class Solution {
     public int solution(int left, int right) {
-        // 사칙연산 값
         int answer = 0;
         
-        // 함수 활용
-        // left부터 right까지 약수 갯수 구하기
-        // 짝수는 더하기, 홀수는 빼기 
         for(int num = left; num <= right; num++){
-            int countsDiv = countDiv(num);
-            // 짝수인 경우
-            if(countsDiv % 2 == 0 ){
+            // 약수 개수 구하기
+            int counts = countDiv(num);
+            // 약수가 짝수인 경우
+            if(counts % 2 == 0){
                 answer += num;
             }
             else {
@@ -19,23 +16,21 @@ class Solution {
         
         return answer;
     }
-    
-    // 약수 개수 세는 함수
-    private int countDiv (int num){
+    // 약수의 개수 세기
+    private int countDiv (int a){
         // 약수의 개수
         int count = 0;
-        //제곱까지 
-        for(int i = 1; i<= Math.sqrt(num); i++){
-            // i로 나누어 떨어지면 약수
-            if(num % i ==0){
+        // 제곱해서 계산하기
+        for(int i = 1; i<=Math.sqrt(a); i++){
+            // i가 약수인 경우
+            if(a % i == 0){
                 count++;
-                // i와 num/i가 서로 다르면 약수
-                if(i != num/i){
+                // i와 a/i가 다른 경우
+                if(i != a/i){
                     count++;
                 }
             }
         }
-        // 약수의 개수 반환하기
         return count;
-    }
+    } 
 }
