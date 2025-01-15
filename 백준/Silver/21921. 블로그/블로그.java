bb3adcc267a06173be60dc9fd5a459673b6fe7a8
jeongeungyeong <150095257+jeongeungyeong@users.noreply.github.com>
2025-01-15 19:58:47 +0900

    [Silver III] Title: 블로그, Time: 316 ms, Memory: 40380 KB -BaekjoonHub

--- 백준/Silver/21921. 블로그/블로그.java
+++ 백준/Silver/21921. 블로그/블로그.java
@@ -1,46 +1,48 @@
 import java.io.*;
 
-public class Main {
+public class Main{
     public static void main(String[] args) throws IOException {
-        // 입력 받기
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // 첫번째 줄 입력
         String[] firstLine = br.readLine().split(" ");
-        int n = Integer.parseInt(firstLine[0]); // 지난 일수
-        int x = Integer.parseInt(firstLine[1]); // 기간 X일
+        int n = Integer.parseInt(firstLine[0]);
+        int x = Integer.parseInt(firstLine[1]);
         
-        String[] visitorInput = br.readLine().split(" "); // 일별 방문자 수
-        int[] visitors = new int[n]; // 일별 방문자 수 배열
-        for (int i = 0; i < n; i++) {
+        // 두번째 줄 입력
+        String[] visitorInput = br.readLine().split(" ");
+        int[] visitors = new int[n];
+        for(int i = 0; i < n; i++){
             visitors[i] = Integer.parseInt(visitorInput[i]);
         }
-
+        
         int maxVisit = 0; // 최대 방문자 수
         int count = 0; // 최대 방문자 수를 가진 기간의 수
-        int sumVisit = 0; // 현재 기간 X일 동안의 방문자 수 합
-
-        // 슬라이딩 윈도우 방식 (투 포인터 방식)
-        int start = 0; // start 포인터
-        for (int end = 0; end < n; end++) {
-            // end 포인터를 하나씩 증가시키며 방문자 수 합을 구함
-            sumVisit += visitors[end];
-            
-            // 기간 X일이 되면 start 포인터를 이동시키며 합을 갱신
-            if (end - start + 1 == x) {
-                // 최대값 갱신
-                if (sumVisit > maxVisit) {
-                    maxVisit = sumVisit;
-                    count = 1; // 새로운 최대값 발견
-                } else if (sumVisit == maxVisit) {
-                    count++; // 같은 최대값 기간이 또 발견
+        int sumVisit = 0; // 현재 기간 x일 동안의 방문자 수 합
+        
+        // 슬라이딩 윈도우 방식
+        for(int i = 0; i < n-x+1; i++){
+            // 첫번째 구간 합 계산
+           if(i == 0){
+               for(int j = 0; j < x; j++){
+                   sumVisit += visitors[j];
+               }
+               maxVisit = sumVisit;
+               count = 1;
+           }
+            else {
+                // 윈도우를 오른쪽으로 한 칸 이동
+                sumVisit = sumVisit - visitors[i-1] + visitors [i+x-1];
+                
+                if(sumVisit == maxVisit){
+                    count++;
                 }
-
-                // start 포인터를 하나 증가시키며 다음 구간으로 이동
-                sumVisit -= visitors[start];
-                start++;
+                else if(sumVisit > maxVisit){
+                    maxVisit = sumVisit;
+                    count = 1;
+                }
             }
         }
-
-        // 결과 출력
+       // 결과 출력
         if (maxVisit == 0) {
             System.out.println("SAD");
         } else {
